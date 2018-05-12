cfd3040d5d2cf205ef151759c812ef3f54c91ffd
dewie102 <zackariah.romasz@yahoo.com>
2018-05-12 15:51:30 -0400

    Had to fix where players could click the same spot that was already taken. Now just to do more visual beautification.

--- TicTacToe/GameForm.cs
+++ TicTacToe/GameForm.cs
@@ -12,7 +12,7 @@ namespace TicTacToe {
         private GroupBox gameBoardGB;
         private int numOfTurns = 0;
 
-        private char[] gamePieces = {' ', 'X', 'O'};
+        private char[] gamePieces = { ' ', 'X', 'O' };
 
         private int[,] winningConditions = new int[,]
             { { 0, 1, 2 },
@@ -37,84 +37,102 @@ namespace TicTacToe {
         }
 
         private void lbl0_Click(object sender, EventArgs e) {
-            MarkBoard(0, "lbl0");
-            if(CheckForWinningCondition()) {
-                ShowEndForm(currPlayer.PlayerName);
-            }
+            if (CheckProperMove(0)) {
+                MarkBoard(0, "lbl0");
+                if (CheckForWinningCondition()) {
+                    ShowEndForm(currPlayer.PlayerName);
+                }
 
-            ChangeTurns();
+                ChangeTurns();
+            }
         }
 
         private void lbl1_Click(object sender, EventArgs e) {
-            MarkBoard(1, "lbl1");
-            if(CheckForWinningCondition()) {
-                ShowEndForm(currPlayer.PlayerName);
-            }
+            if (CheckProperMove(1)) {
+                MarkBoard(1, "lbl1");
+                if (CheckForWinningCondition()) {
+                    ShowEndForm(currPlayer.PlayerName);
+                }
 
-            ChangeTurns();
+                ChangeTurns();
+            }
         }
 
         private void lbl2_Click(object sender, EventArgs e) {
-            MarkBoard(2, "lbl2");
-            if(CheckForWinningCondition()) {
-                ShowEndForm(currPlayer.PlayerName);
-            }
+            if (CheckProperMove(2)) {
+                MarkBoard(2, "lbl2");
+                if (CheckForWinningCondition()) {
+                    ShowEndForm(currPlayer.PlayerName);
+                }
 
-            ChangeTurns();
+                ChangeTurns();
+            }
         }
 
         private void lbl3_Click(object sender, EventArgs e) {
-            MarkBoard(3, "lbl3");
-            if(CheckForWinningCondition()) {
-                ShowEndForm(currPlayer.PlayerName);
-            }
+            if (CheckProperMove(3)) {
+                MarkBoard(3, "lbl3");
+                if (CheckForWinningCondition()) {
+                    ShowEndForm(currPlayer.PlayerName);
+                }
 
-            ChangeTurns();
+                ChangeTurns();
+            }
         }
 
         private void lbl4_Click(object sender, EventArgs e) {
-            MarkBoard(4, "lbl4");
-            if(CheckForWinningCondition()) {
-                ShowEndForm(currPlayer.PlayerName);
-            }
+            if (CheckProperMove(4)) {
+                MarkBoard(4, "lbl4");
+                if (CheckForWinningCondition()) {
+                    ShowEndForm(currPlayer.PlayerName);
+                }
 
-            ChangeTurns();
+                ChangeTurns();
+            }
         }
 
         private void lbl5_Click(object sender, EventArgs e) {
-            MarkBoard(5, "lbl5");
-            if(CheckForWinningCondition()) {
-                ShowEndForm(currPlayer.PlayerName);
-            }
+            if (CheckProperMove(5)) {
+                MarkBoard(5, "lbl5");
+                if (CheckForWinningCondition()) {
+                    ShowEndForm(currPlayer.PlayerName);
+                }
 
-            ChangeTurns();
+                ChangeTurns();
+            }
         }
 
         private void lbl6_Click(object sender, EventArgs e) {
-            MarkBoard(6, "lbl6");
-            if(CheckForWinningCondition()) {
-                ShowEndForm(currPlayer.PlayerName);
-            }
+            if (CheckProperMove(6)) {
+                MarkBoard(6, "lbl6");
+                if (CheckForWinningCondition()) {
+                    ShowEndForm(currPlayer.PlayerName);
+                }
 
-            ChangeTurns();
+                ChangeTurns();
+            }
         }
 
         private void lbl7_Click(object sender, EventArgs e) {
-            MarkBoard(7, "lbl7");
-            if(CheckForWinningCondition()) {
-                ShowEndForm(currPlayer.PlayerName);
-            }
+            if (CheckProperMove(7)) {
+                MarkBoard(7, "lbl7");
+                if (CheckForWinningCondition()) {
+                    ShowEndForm(currPlayer.PlayerName);
+                }
 
-            ChangeTurns();
+                ChangeTurns();
+            }
         }
 
         private void lbl8_Click(object sender, EventArgs e) {
-            MarkBoard(8, "lbl8");
-            if(CheckForWinningCondition()) {
-                ShowEndForm(currPlayer.PlayerName);
-            }
+            if (CheckProperMove(8)) {
+                MarkBoard(8, "lbl8");
+                if (CheckForWinningCondition()) {
+                    ShowEndForm(currPlayer.PlayerName);
+                }
 
-            ChangeTurns();
+                ChangeTurns();
+            }
         }
 
         private void ResetGameBoard() {
@@ -124,25 +142,33 @@ namespace TicTacToe {
                 lbl.Text = "";
             }
 
-            for(int i = 0; i < gameBoard.Length; i++) {
+            for (int i = 0; i < gameBoard.Length; i++) {
                 gameBoard[i] = '\0';
             }
 
             DisplayCurrPlayer();
         }
 
+        private bool CheckProperMove(int Index) {
+            if (gameBoard[Index] != 'X' && gameBoard[Index] != 'O') {
+                return true;
+            }
+
+            return false;
+        }
+
         private void MarkBoard(int index, string labelName) {
-            if(index < gameBoard.Length) {
+            if (index < gameBoard.Length) {
                 char symbol = gamePieces[(int)currPlayer.PlayerSymbol];
-                gameBoard[index] = symbol;
-                gameBoardGB.Controls[labelName].Text = symbol.ToString();
+                gameBoard[index] = symbol; // Change data map to proper symbol
+                gameBoardGB.Controls[labelName].Text = symbol.ToString(); // Change graphical map to proper symbol
             }
         }
 
         private void ChangeTurns() {
-            if(currPlayer == GameData.player1) {
+            if (currPlayer == GameData.player1) {
                 currPlayer = GameData.player2;
-            } else if(currPlayer == GameData.player2) {
+            } else if (currPlayer == GameData.player2) {
                 currPlayer = GameData.player1;
             }
 
@@ -150,7 +176,7 @@ namespace TicTacToe {
         }
 
         private void DisplayCurrPlayer() {
-            if(currPlayer == GameData.player1) {
+            if (currPlayer == GameData.player1) {
                 lblPlayer2.BackColor = SystemColors.Control;
                 lblPlayer1.BackColor = SystemColors.ActiveCaption;
             } else if (currPlayer == GameData.player2) {
@@ -166,10 +192,10 @@ namespace TicTacToe {
 
             numOfTurns++;
 
-            for(int r = 0; r <= winningConditions.GetUpperBound(0); r++) {
-                for(int c = 0; c <= winningConditions.GetUpperBound(1); c++) {
-                    if(gameBoard[winningConditions[r,c]] == matchSymbol) {
-                        if(++matchCount == 3) {
+            for (int r = 0; r <= winningConditions.GetUpperBound(0); r++) {
+                for (int c = 0; c <= winningConditions.GetUpperBound(1); c++) {
+                    if (gameBoard[winningConditions[r, c]] == matchSymbol) {
+                        if (++matchCount == 3) {
                             result = true;
                             break;
                         }
@@ -178,7 +204,7 @@ namespace TicTacToe {
                 matchCount = 0;
             }
 
-            if(numOfTurns == 9 && result == false) {
+            if (numOfTurns == 9 && result == false) {
                 ShowEndForm("", result);
             }
 
